a61e8f33cf9ab6d2b9370af8a30d1db900955fc5
neilxstc <neilmxstc@gmail.com>
2020-07-14 21:36:08 +0300

    Updates

---
+++ precache-manifest.c056bf27dd36bf8dcbe54fae66f7a3a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474f029bf8b7faee4fb0ce4f4ded1706",
+    "url": "/Neil-Michaeli-08-07-2020/index.html"
+  },
+  {
+    "revision": "b174179db47f995811c0",
+    "url": "/Neil-Michaeli-08-07-2020/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "09a72aafc5efc3f79189",
+    "url": "/Neil-Michaeli-08-07-2020/static/css/main.32160c97.chunk.css"
+  },
+  {
+    "revision": "b174179db47f995811c0",
+    "url": "/Neil-Michaeli-08-07-2020/static/js/2.7dfaaa51.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Neil-Michaeli-08-07-2020/static/js/2.7dfaaa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a72aafc5efc3f79189",
+    "url": "/Neil-Michaeli-08-07-2020/static/js/main.8d5da716.chunk.js"
+  },
+  {
+    "revision": "27b40cfd0d241b531890",
+    "url": "/Neil-Michaeli-08-07-2020/static/js/runtime-main.6a1c0adc.js"
+  }
+]);
